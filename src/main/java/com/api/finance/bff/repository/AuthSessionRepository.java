4f70230b028e5c4180662a2beae0a8be95c611c4
Riosberg Dutra <riosbergduttra@gmail.com>
2026-03-27 22:49:49 -0300

    feat(bff): repository

--- src/main/java/com/api/finance/bff/repository/AuthSessionRepository.java
+++ src/main/java/com/api/finance/bff/repository/AuthSessionRepository.java
@@ -1,4 +1,10 @@
 package com.api.finance.bff.repository;
 
-public interface AuthRepository {
+import com.api.finance.bff.model.AuthSession;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface AuthSessionRepository extends CrudRepository<AuthSession, String> {
+    Optional<AuthSession> findByKeycloakId(String code);
 }
