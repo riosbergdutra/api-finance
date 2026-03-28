a9d484ef0db540c8564c0751c90c7175d7b18b41
Riosberg Dutra <riosbergduttra@gmail.com>
2026-03-27 22:50:43 -0300

    feat(bff): dto

--- src/main/java/com/api/finance/bff/dto/AuthResponseDTO.java
+++ src/main/java/com/api/finance/bff/dto/AuthResponseDTO.java
@@ -1,4 +1,21 @@
 package com.api.finance.bff.dto;
 
-public class AuthResponseDTO {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record AuthResponseDTO(
+        @JsonProperty("access_token")
+        String accessToken,
+
+        @JsonIgnore
+        String sessionId
+) {
+    // CORREÇÃO: O construtor precisa ser assim para não dar erro de recursão
+    public AuthResponseDTO(String accessToken, String sessionId) {
+        // O Java exige que o construtor customizado preencha os campos do record
+        this.accessToken = accessToken;
+        this.sessionId = sessionId;
+    }
 }
