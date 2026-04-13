59d6f9da6956328c1acefa5fa33e15d3f32f78a2
Riosberg Dutra <riosbergduttra@gmail.com>
2026-04-13 15:22:50 -0300

    feat(user): findidbykeycloakid para todos meus outros servicos

--- src/main/java/com/api/finance/user/repository/UserRepository.java
+++ src/main/java/com/api/finance/user/repository/UserRepository.java
@@ -3,10 +3,12 @@ package com.api.finance.user.repository;
 import java.util.Optional;
 import java.util.UUID;
 
+import com.api.finance.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.api.finance.user.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
@@ -17,4 +19,6 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     void deleteByKeycloakId(UUID keycloakId);
     boolean existsByKeycloakId(UUID keycloakId);
     boolean existsByEmail(String email);
+    @Query("SELECT u.id FROM User u WHERE u.keycloakId = :keycloakId")
+    Optional<UUID> findIdByKeycloakId(@Param("keycloakId") UUID keycloakId);
 }
