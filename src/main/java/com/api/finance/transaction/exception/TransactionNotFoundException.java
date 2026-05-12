fdc1479e8c5d2a34126a651d0e4cde4977b51f23
Riosberg Dutra <riosbergduttra@gmail.com>
2026-05-12 14:44:24 -0300

    feat(transaction): exception

---
+++ src/main/java/com/api/finance/transaction/exception/TransactionNotFoundException.java
@@ -0,0 +1,9 @@
+package com.api.finance.transaction.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class TransactionNotFoundException extends RuntimeException {
+    public TransactionNotFoundException(String msg) { super(msg); }
+}
